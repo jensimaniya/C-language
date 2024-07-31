60ab0aeb27f8959922fa99a13bf8f05050df4db6
jensimaniya <jensimaniya@gmail.com>
2024-07-31 14:11:42 +0530

    Datatype,Constant,Variable

---
+++ ch-2/lw-1-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=12,b=6;
+	
+	printf("Addition of %d and %d is %d\n",a,b,a+b);
+	printf("Subtraction of %d and %d is %d\n",a,b,a-b);
+	printf("Multiplication of %d and %d is %d\n",a,b,a*b);
+	printf("Division of %d and %d is %d\n",a,b,a/b);
+	printf("Modulus of %d and %d is %d\n\n",a,b,a%b);
+	printf("%d+%d=%d\n",a,b,a+b);
+	printf("%d-%d=%d\n",a,b,a-b);
+	printf("%d*%d=%d\n",a,b,a*b);
+	printf("%d/%d=%d\n",a,b,a/b);
+	printf("%d#%d=%d\n",a,b,a%b);
+
+}
