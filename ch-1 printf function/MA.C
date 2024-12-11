a5965a57cad4106720a7c84b921ea1ce0eb540ae
jensimaniya <jensimaniya@gmail.com>
2024-12-11 11:26:48 +0530

    C language

---
+++ ch-1 printf function/MA.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+ main()
+  {
+  
+  printf("-----\n");
+  printf("|      |\n");
+  printf("r      |\n");
+  printf("n      |\n");
+  printf("w      |\n");
+  printf("|      |\n");
+  printf("-----\n");
+
+  }
