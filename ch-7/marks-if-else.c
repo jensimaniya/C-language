f1f59c6703e1e5885a35e042b03212080434b9d8
jensimaniya <jensimaniya@gmail.com>
2024-12-12 10:57:50 +0530

    C language

--- ch-7/marks-if-else.c
+++ ch-7/marks-if-else.c
@@ -8,31 +8,31 @@ int main()
 
         if(marks>=91 && marks<=100)
             {
-                printf("greada:a");
+                printf("greada:a\n");
                 printf(" excellent work!!");
                 
             }
         else if(marks>=81 && marks<=90)
             {
-                printf("gread:b");
+                printf("gread:b\n");
                 printf("well done");
                 
             }
         else if (marks>=71 && marks<=80)
             {
-                printf("gread :c");
+                printf("gread :c\n");
                 printf("good job");
                 
             }
         else if (marks>=61 && marks<=70)
             {
-                printf("gread :d");
+                printf("gread :d\n");
                 printf("you passed");
                 
             }
         else if (marks>=20 && marks<=34)
             {
-                printf("gread :e");
+                printf("gread :e\n");
                 printf("fail");
                
             }
